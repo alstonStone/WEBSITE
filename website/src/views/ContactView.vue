dec169472189b8e073f5e1827450874abaa230c3
alstonStone <alstonStone0@gmail.com>
2023-06-15 12:07:28 -0400

    contact changed from form to javascript method

--- website/src/views/ContactView.vue
+++ website/src/views/ContactView.vue
@@ -1,12 +1,30 @@
 <template>
     <div class="contact">
-        <form action="http://localhost:7070/contact">
-            <label for="email">Enter your email:</label>
-            <input type="email" id="email" name="email">
-            <input type="submit" value="Submit">
-        </form>
+        <label for="email">Enter your email:</label>
+        <input type="email" name="email" v-model="email">
+        <button v-on:click="addUser()">Send</button>
     </div>
   </template>
+
+<script>
+  export default {
+
+    data(){
+        return {
+          email:'',
+        };
+    },
+    methods:{
+      addUser(){
+        console.warn(this.email);
+      }
+    }
+  };
+
+
+</script>
+
+
   
   <style>
  
